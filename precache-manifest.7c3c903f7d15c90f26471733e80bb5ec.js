398e08213dea3191d03ebfb54afd7218623262f1
Kyle PC <kyle.dahl201@gmail.com>
2020-08-31 01:14:19 -0500

    Updates

---
+++ precache-manifest.7c3c903f7d15c90f26471733e80bb5ec.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "14ff3a1fe7cb6287c564b19a91ed4dbc",
+    "url": "/react-pokedex/index.html"
+  },
+  {
+    "revision": "2df82b9ea3611045d82f",
+    "url": "/react-pokedex/static/css/main.dde51e68.chunk.css"
+  },
+  {
+    "revision": "cf2e9e6985f095d4b992",
+    "url": "/react-pokedex/static/js/2.cf96ab51.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-pokedex/static/js/2.cf96ab51.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2df82b9ea3611045d82f",
+    "url": "/react-pokedex/static/js/main.73b30fef.chunk.js"
+  },
+  {
+    "revision": "a6f53391d15d59d26871",
+    "url": "/react-pokedex/static/js/runtime-main.0876dcbc.js"
+  }
+]);
